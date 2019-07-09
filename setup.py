542f6313eb1352f43122999a41de6258f6241fb5
asian-code <52612730+asian-code@users.noreply.github.com>
2019-07-09 10:55:17 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,27 @@
 
 import os
 import time
-os.system("pip3 install scapy")
 
-os.system("sudo mv simple-scan /usr/share")# folder
-print("[+] moved simple-scan folder to /usr/share")
-os.system("sudo mv simplescan.desktop /usr/share/applications/")#.desktop file
-print("[+] moved desktop to /usr/share/applications")
-os.system("sudo mv simplescan /usr/bin")#bash file
-print("[+] moved bash file to /usr/bin")
-time.sleep(3)
-print("[+] Setup is complete")
+had_error = False
+try:
+    os.system("pip3 install scapy")
+
+    os.system("sudo mv simple-scan /usr/share")  # folder
+    print("[+] moved simple-scan folder to /usr/share")
+
+    os.system("sudo mv simplescan.desktop /usr/share/applications/")  # .desktop file
+    print("[+] moved desktop to /usr/share/applications")
+
+    os.system("sudo mv simplescan /usr/bin")  # bash file
+    print("[+] moved bash file to /usr/bin")
+
+    time.sleep(3)
+
+except:
+    had_error = True
+    raise
+finally:
+    if had_error:
+        print("[-] Setup Failed ")
+    else:
+        print("[+] Setup is complet
