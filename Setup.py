d87b905f403572d87ed86a1b5b1dc0e8320f2b33
asian-code <52612730+asian-code@users.noreply.github.com>
2019-07-08 21:14:20 -0400

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -4,7 +4,10 @@ import os
 import time
 
 os.system("sudo mv simple-scan /usr/share")# folder
+print("[+] moved simple-scan folder to /usr/share")
 os.system("sudo mv simplescan.desktop /usr/share/applications/")#.desktop file
+print("[+] moved desktop to /usr/share/applications")
 os.system("sudo mv simplescan /usr/bin")#bash file
+print("[+] moved bash file to /usr/bin")
 time.sleep(3)
 print("[+] Setup is complete")
