b3d0a5c9966a574c1269f2e23212c97f770b65d8
asian-code <52612730+asian-code@users.noreply.github.com>
2019-07-09 12:50:23 -0400

    Update setup.py
    it should write to a file the location of installation folder

--- setup.py
+++ setup.py
@@ -5,6 +5,33 @@ import time
 import subprocess
 
 had_error = False
+
+
+def get_current_dir():
+    save_location = str(subprocess.check_output(["pwd"]))
+    # save directory to txt file in the simple-scan folder
+    # delete this folder later in uninstaller.py
+    folder = save_location.split("/")
+    folder.remove("b'")  # this is needed to remove the first element in the list
+    folder[len(folder) - 1] = folder[len(folder) - 1].split("\\")[0]  # removes the \n in the last element
+    result = ""
+    for i in folder:
+        result += "/" + i
+    return result
+
+
+def save_folder_location(location):
+    try:
+        file = open(location + "/location.txt", "w")
+        file.write(location)
+    except:
+        print("[-] Error writing to file")
+        had_error = True
+        raise
+    finally:
+        file.close()
+
+
 try:
     subprocess.call("pip3 install scapy", shell=True)
 
@@ -17,16 +44,8 @@ try:
     subprocess.call("sudo mv simplescan /usr/bin", shell=True)  # bash file
     print("[+] moved bash file to /usr/bin")
 
-    # get current direcotry
-    save_location = str(subprocess.check_output(["pwd"]))
-    # save directory to file in the folder
-    # delete this folder later in uninstaller.py
-    folder = save_location.split("/")
-    folder.remove("b'")  # this is needed to remove the first element in the list
-    folder[len(folder) - 1] = folder[len(folder) - 1].split("\\")[0]
-    for i in folder:
-        print(i)
-    # print(save_location)
+    save_folder_location(get_current_dir())
+
     time.sleep(3)
 except:
     had_error = True
