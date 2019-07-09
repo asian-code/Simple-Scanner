d5dc029c2104ed6493e052dd8152a2f5a90826dd
asian-code <52612730+asian-code@users.noreply.github.com>
2019-07-08 21:01:14 -0400

    Update simplescanUninstaller.py

--- simplescanUninstaller.py
+++ simplescanUninstaller.py
@@ -3,8 +3,11 @@
 import os
 import time
 
-os.system("rm -rf /usr/share/simple-scan")  # folder
-os.system("rm /usr/share/applications/simplescan.desktop")  # desktop file
-os.system("rm /usr/bin/simplescan")  # bash file
+os.system("sudo rm -rf /usr/share/simple-scan")  # folder
+print("[+] removed simple-scan folder in /usr/share/simple-scan")
+os.system("sudo rm -rf /usr/share/applications/simplescan.desktop")  # desktop file
+print("[+] removed desktop file in /usr/share/applications/simplescan.desktop")
+os.system("sudo rm -rf /usr/bin/simplescan")  # bash file
+print("[+] removed bash file /usr/bin/simplescan")
 time.sleep(3)
 print("[+] Uninstall is complete")
