439c4b6ff2227bb49609cf8f0d2c0a22ff941127
asian-code <52612730+asian-code@users.noreply.github.com>
2019-07-09 11:24:26 -0400

    Update simplescanUninstaller.py

--- simplescanUninstaller.py
+++ simplescanUninstaller.py
@@ -3,11 +3,23 @@
 import os
 import time
 
-os.system("sudo rm -rf /usr/share/simple-scan")  # folder
-print("[+] removed simple-scan folder in /usr/share/simple-scan")
-os.system("sudo rm -rf /usr/share/applications/simplescan.desktop")  # desktop file
-print("[+] removed desktop file in /usr/share/applications/simplescan.desktop")
-os.system("sudo rm -rf /usr/bin/simplescan")  # bash file
-print("[+] removed bash file /usr/bin/simplescan")
-time.sleep(3)
-print("[+] Uninstall is complete")
+had_error = False
+try:
+    os.system("sudo rm -rf /usr/share/simple-scan")  # folder
+    print("[+] removed simple-scan folder in /usr/share/simple-scan")
+
+    os.system("sudo rm -rf /usr/share/applications/simplescan.desktop")  # desktop file
+    print("[+] removed desktop file in /usr/share/applications/simplescan.desktop")
+
+    os.system("sudo rm -rf /usr/bin/simplescan")  # bash file
+    print("[+] removed bash file /usr/bin/simplescan")
+
+    time.sleep(3)
+except:
+    had_error = True
+    raise
+finally:
+    if had_error:
+        print("[-] Unable to uninstall Simple-scan due to an error")
+    else:
+        print("[+] Uninstall is complete, no errors !")
