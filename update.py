4d313817245bdaeea744320172e4e80725cda2b2
asian-code <52612730+asian-code@users.noreply.github.com>
2019-07-10 14:38:30 -0400

    Update update.py

--- update.py
+++ update.py
@@ -4,11 +4,26 @@ import os
 import sys
 
 had_error = False
+
+
+def get_current_dir():
+    save_location = str(subprocess.check_output(["pwd"]))
+    # save directory to txt file in the simple-scan folder
+    # delete this folder later in uninstaller.py
+    folder = save_location.split("/")
+    folder.pop(0)  # this is needed to remove the first element in the list
+    folder[len(folder) - 1] = folder[len(folder) - 1].split("\\")[0]  # removes the \n in the last element
+    result = ""
+    for i in folder:
+        result += "/" + i
+        return result
+
+
 try:
     if os.path.exists("location.txt") == False:
         raise Exception("Missing location.txt file")
     else:
-        print("location.txt is good")
+        print("location.txt is located")
 except:
     print("[!] Error -Simple scan was never properly installed to update,\n\tMissing location.txt file")
     sys.exit()
@@ -25,19 +40,20 @@ new_location = ""
 for i in old_location:
     new_location += "/{}".format(i)
 
-# print(new_location)
+print("[+] Location to install: " + new_location)
 try:
     # uninstalls current version
     subprocess.call("sudo python3 uninstaller.py", shell=True)
-    # installs new version
-    subprocess.call("cd {}".format(new_location), shell=True)
-    subprocess.call("git clone https://github.com/asian-code/Simple-Scanner.git", shell=True)
 
-    subprocess.call("cd {}".format(location), shell=True)
-    # Add chmod
-    os.chmod(location, os.stat.S_IXUSR | os.stat.S_IXGRP | os.stat.S_IXOTH)
-    # run setup.py
-    subprocess.call("sudo python3 setup.py", shell=True)
+    # installs new version
+    # subprocess.call("cd {}".format(new_location), shell=True)
+    subprocess.call("git clone https://github.com/asian-code/Simple-Scanner.git", shell=True)
+    curr_location = get_current_dir()
+    print("current location:" + curr_location)
+
+    # os.chmod(location, os.stat.S_IXUSR | os.stat.S_IXGRP | os.stat.S_IXOTH) # chmod
+
+    # subprocess.call("sudo python3 setup.py", shell=True)#setup new version
 except:
     had_error = True
 
