77c0bde3705e418e808d24e6917a6cfd9094d22a
asian-code <52612730+asian-code@users.noreply.github.com>
2019-07-10 11:09:34 -0400

    Update uninstaller.py

--- uninstaller.py
+++ uninstaller.py
@@ -11,7 +11,7 @@ try:
     os.system("sudo rm -rf /usr/share/applications/simplescan.desktop")  # desktop file
     print("[+] removed desktop file in /usr/share/applications/simplescan.desktop")
 
-    os.system("sudo rm -rf /usr/bin/simplescan")  # bash file
+    os.system("sudo rm -rf /usr/bin/simplescanner")  # bash file
     print("[+] removed bash file /usr/bin/simplescan")
 
     # removes the folder where simple scan installation folder is located
