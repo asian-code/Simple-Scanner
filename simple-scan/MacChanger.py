282f012c858587a2a3baa25cfd267cb7c9c8193f
asian-code <52612730+asian-code@users.noreply.github.com>
2019-07-11 18:53:11 -0400

    Update MacChanger.py

--- simple-scan/MacChanger.py
+++ simple-scan/MacChanger.py
@@ -2,18 +2,20 @@
 import subprocess  # cmd commands module
 import optparse  # external input with script through cmd EXAMPLE: macChanger.py -i eth0 -m 00:11:22:33:44:55
 import re  # regular expression
+
 red = '\033[31m'
 green = '\033[92m'
 rr = '\033[0m'  # reset
 bold = '\033[01m'
+lblue = '\033[94m'
 
 # my Original mac eth0: 08:00:27:89:03:db
 
 def show_info(interface="None", new_mac="None"):
-    txt = green+bold+"--------------------------------- \n" \
-          "| Interface  >  {}\n" \
-          "| New Mac    >  {}\n" \
-          "--------------------------------- \n"+rr
+    txt = lblue + bold + "--------------------------------- \n" \
+                         "| Interface  >  {}\n" \
+                         "| New Mac    >  {}\n" \
+                         "--------------------------------- \n" + rr
     print(txt.format(interface, new_mac))
 
 
@@ -59,10 +61,10 @@ def main():
         print(" Current MAC address > " + get_mac(value.inface))
         change_mac(value.inface, value.nmac)
         if old_mac == get_mac(value.inface):
-            print(red+bold+"[-] Error Changing the MAC address, Old MAC and new MAC are the same"+rr)
+            print(red + bold + "[-] Error Changing the MAC address, Old MAC and new MAC are the same" + rr)
         else:
-            print(green+bold+"[ OK ] Change Complete "+rr)
+            print(green + bold + "[ OK ] Change Complete " + rr)
     else:
-        print(red+bold+"[!] Error - Could not find MAC address for this interface"+rr)
 
-# print(type(get_args()))
+
+print(red + bold + "[!] Error - Could not find MAC address for this interface" + rr)
